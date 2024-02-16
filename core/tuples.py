bb1112836200a8e7eba150331006a72eb52a211f
Miguel Linares <ifmlinares@gmail.com>
2024-02-16 09:24:11 -0400

    Cambios

---
+++ core/tuples.py
@@ -0,0 +1,181 @@
+from datetime import datetime, time
+
+# Tupla de 9:00AM - 7:00PM
+HOUR_CHOICES = [(time(hour=i, minute=0).strftime('%I:%M %p'), time(hour=i, minute=0).strftime('%I:%M %p')) for i in range(9, 20)]
+
+# Tupla de 8:00AM - 7:00PM
+HOUR_CHOICES_SP = [(time(hour=i, minute=0).strftime('%I:%M %p'), time(hour=i, minute=0).strftime('%I:%M %p')) for i in range(8, 20)]
+
+# Tupla de 8:30AM - 7:30PM
+HOUR_CHOICES_GP = [(time(hour=i, minute=j).strftime('%I:%M %p'), time(hour=i, minute=j).strftime('%I:%M %p')) for i in range(8, 21) for j in (30,) if not (i == 8 and j == 0)]
+
+# Tupla de 7:00AM - 6:00PM
+HOUR_CHOICES_LG = [(time(hour=i, minute=0).strftime('%I:%M %p'), time(hour=i, minute=0).strftime('%I:%M %p')) for i in range(8, 20)]
+
+# Tupla de 9:05AM - 7:05PM
+HOUR_CHOICES_RC = [(time(hour=i, minute=5).strftime('%I:%M %p'), time(hour=i, minute=5).strftime('%I:%M %p')) for i in range(9, 20)]
+
+# Lista de animales para ChanceAnimalitos
+ANIMALITO_CHOICES = [
+    ("00", "Ballena"),
+    ("0", "Delfín"),
+    ("1", "Carnero"),
+    ("2", "Toro"),
+    ("3", "Ciempies"),
+    ("4", "Alacran"),
+    ("5", "León"),
+    ("6", "Sapo"),
+    ("7", "Loro"),
+    ("8", "Ratón"),
+    ("9", "Águila"),
+    ("10", "Tigre"),
+    ("11", "Gato"),
+    ("12", "Caballo"),
+    ("13", "Mono"),
+    ("14", "Paloma"),
+    ("15", "Zorro"),
+    ("16", "Oso"),
+    ("17", "Pavo"),
+    ("18", "Burro"),
+    ("19", "Cabra"),
+    ("20", "Cochino"),
+    ("21", "Gallo"),
+    ("22", "Camello"),
+    ("23", "Cebra"),
+    ("24", "Iguana"),
+    ("25", "Gallina"),
+    ("26", "Vaca"),
+    ("27", "Perro"),
+    ("28", "Zamuro"),
+    ("29", "Elefante"),
+    ("30", "Caimán"),
+    ("31", "Lapa"),
+    ("32", "Ardilla"),
+    ("33", "Pescado"),
+    ("34", "Venado"),
+    ("35", "Jirafa"),
+    ("36", "Culebra")
+]
+
+# Lista Para la granja plus
+ANIMALITO_GRANA_PLUS_CHOICES = [
+    ("1", "Carnero"),
+    ("2", "Toro"),
+    ("3", "Ciempies"),
+    ("4", "Alacran"),
+    ("5", "León"),
+    ("6", "Rana"),
+    ("7", "Perico"),
+    ("8", "Ratón"),
+    ("9", "Águila"),
+    ("10", "Tigre"),
+    ("11", "Gato"),
+    ("12", "Caballo"),
+    ("13", "Mono"),
+    ("14", "Paloma"),
+    ("15", "Zorro"),
+    ("16", "Oso"),
+    ("17", "Pavo"),
+    ("18", "Burro"),
+    ("19", "Chivo"),
+    ("20", "Cochino"),
+    ("21", "Gallo"),
+    ("22", "Camello"),
+    ("23", "Cebra"),
+    ("24", "Iguana"),
+    ("25", "Gallina"),
+    ("26", "Vaca"),
+    ("27", "Perro"),
+    ("28", "Zamuro"),
+    ("29", "Elefante"),
+    ("30", "Caimán"),
+    ("31", "Lapa"),
+    ("32", "Ardilla"),
+    ("33", "Pescado"),
+    ("34", "Venado"),
+    ("35", "Jirafa"),
+    ("36", "Culebra")
+]
+
+# La grajita
+ANIMALITO_GRANJITA_LOTTO_ACTIVO_SELVA_PLUS_CHOICES = [
+    ("00", "Ballena"),
+    ("0", "Delfín"),
+    ("1", "Carnero"),
+    ("2", "Toro"),
+    ("3", "Ciempies"),
+    ("4", "Alacran"),
+    ("5", "León"),
+    ("6", "Rana"),
+    ("7", "Perico"),
+    ("8", "Ratón"),
+    ("9", "Águila"),
+    ("10", "Tigre"),
+    ("11", "Gato"),
+    ("12", "Caballo"),
+    ("13", "Mono"),
+    ("14", "Paloma"),
+    ("15", "Zorro"),
+    ("16", "Oso"),
+    ("17", "Pavo"),
+    ("18", "Burro"),
+    ("19", "Chivo"),
+    ("20", "Cochino"),
+    ("21", "Gallo"),
+    ("22", "Camello"),
+    ("23", "Cebra"),
+    ("24", "Iguana"),
+    ("25", "Gallina"),
+    ("26", "Vaca"),
+    ("27", "Perro"),
+    ("28", "Zamuro"),
+    ("29", "Elefante"),
+    ("30", "Caimán"),
+    ("31", "Lapa"),
+    ("32", "Ardilla"),
+    ("33", "Pescado"),
+    ("34", "Venado"),
+    ("35", "Jirafa"),
+    ("36", "Culebra")
+]
+
+# lA RICACHONA
+ANIMALITO_LA_RICACHONA_CHOICES = [
+    ("00", "Ballena"),
+    ("1", "Carnero"),
+    ("2", "Toro"),
+    ("3", "Ciempies"),
+    ("4", "Alacran"),
+    ("5", "León"),
+    ("6", "Rana"),
+    ("7", "Perico"),
+    ("8", "Ratón"),
+    ("9", "Águila"),
+    ("10", "Tigre"),
+    ("11", "Gato"),
+    ("12", "Caballo"),
+    ("13", "Mono"),
+    ("14", "Paloma"),
+    ("15", "Zorro"),
+    ("16", "Oso"),
+    ("17", "Pavo"),
+    ("18", "Burro"),
+    ("19", "Chivo"),
+    ("20", "Cochino"),
+    ("21", "Gallo"),
+    ("22", "Camello"),
+    ("23", "Cebra"),
+    ("24", "Iguana"),
+    ("25", "Gallina"),
+    ("26", "Vaca"),
+    ("27", "Perro"),
+    ("28", "Zamuro"),
+    ("29", "Elefante"),
+    ("30", "Caimán"),
+    ("31", "Lapa"),
+    ("32", "Ardilla"),
+    ("33", "Pescado"),
+    ("34", "Venado"),
+    ("35", "Jirafa"),
+    ("36", "Culebra")
+]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,29 @@
+from datetime import time
+from collections import defaultdict
 from django.shortcuts import render
 from django.views.generic import View
+from django.utils import timezone
+from .models import *
 # Create your views here.
 
 class LotteryView(View):
     def get(self, request):
-        # Lógica de tu vista aquí
-        return render(request, 'lotoview/index.html')
+        # Obtén la fecha actual
+        today = timezone.now().date()
+
+        # Realiza la consulta para obtener todos los resultados de hoy
+        today_results = ChanceAnimalitos.objects.filter(date_sort=today)
+
+        # Crea un diccionario con todos los horarios posibles
+        results_dict = defaultdict(lambda: "-----")
+        for hour, _ in HOUR_CHOICES:
+            results_dict[hour]
+
+        # Llena el diccionario con los resultados de hoy
+        for result in today_results:
+            results_dict[result.hour_sort] = result.get_animalito_display()
+
+        # Pasa los resultados al contexto de la plantilla
+        context = {'results': dict(results_dict)}
+
+        return render(request, 'lotoview/index.html', context)
