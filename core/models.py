78eb035f0a8f875d4bc90da7eaa94f482361355c
Miguel Linares <ifmlinares@gmail.com>
2024-02-16 18:11:09 -0400

    Changes tuples

--- core/models.py
+++ core/models.py
@@ -2,7 +2,19 @@ from django.db import models
 from datetime import datetime
 from django.conf import settings
 from os.path import join
-from .tuples import *
+from .tuples import (
+    HOUR_CHOICES,
+    HOUR_CHOICES_SP,
+    HOUR_CHOICES_GP,
+    HOUR_CHOICES_LG,
+    HOUR_CHOICES_RC,
+    HOUR_CHOICES_GA,
+    ANIMALITO_CHOICES,
+    ANIMALITO_GRANA_PLUS_CHOICES,
+    ANIMALITO_GRANJITA_LOTTO_ACTIVO_SELVA_PLUS_CHOICES,
+    ANIMALITO_LA_RICACHONA_CHOICES,
+    ANIMALITO_GUACHARO_ACTIVO_CHOICES,
+)
 
 # Create your models here.
 class ChanceAnimalitos(models.Model):
