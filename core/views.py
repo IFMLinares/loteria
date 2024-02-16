af8a8f930ed604cd45162b8213c449a1315ba90e
David Alvarado <alvarado.davidjm@gmail.com>
2024-02-16 10:44:45 -0400

    css

--- core/views.py
+++ core/views.py
@@ -8,22 +8,22 @@ from .models import *
 
 class LotteryView(View):
     def get(self, request):
-        # Obtén la fecha actual
-        today = timezone.now().date()
+        # # Obtén la fecha actual
+        # today = timezone.now().date()
 
-        # Realiza la consulta para obtener todos los resultados de hoy
-        today_results = ChanceAnimalitos.objects.filter(date_sort=today)
+        # # Realiza la consulta para obtener todos los resultados de hoy
+        # today_results = ChanceAnimalitos.objects.filter(date_sort=today)
 
-        # Crea un diccionario con todos los horarios posibles
-        results_dict = defaultdict(lambda: "-----")
-        for hour, _ in HOUR_CHOICES:
-            results_dict[hour]
+        # # Crea un diccionario con todos los horarios posibles
+        # results_dict = defaultdict(lambda: "-----")
+        # for hour, _ in HOUR_CHOICES:
+        #     results_dict[hour]
 
-        # Llena el diccionario con los resultados de hoy
-        for result in today_results:
-            results_dict[result.hour_sort] = result.get_animalito_display()
+        # # Llena el diccionario con los resultados de hoy
+        # for result in today_results:
+        #     results_dict[result.hour_sort] = result.get_animalito_display()
 
-        # Pasa los resultados al contexto de la plantilla
-        context = {'results': dict(results_dict)}
+        # # Pasa los resultados al contexto de la plantilla
+        # context = {'results': dict(results_dict)}
 
-        return render(request, 'lotoview/index.html', context)
+        return render(request, 'lotoview/index.html')
