0002861a803f2764eec60bedaa20ca387f0950e4
Miguel Linares <ifmlinares@gmail.com>
2024-09-16 20:55:22 -0400

    Delete

---
+++ core/delete.py
@@ -0,0 +1,59 @@
+from django.core.management.base import BaseCommand
+from datetime import timedelta
+from django.utils import timezone
+
+from .models import (
+    ChanceAnimalitos,
+    FruitaGana,
+    GranjaMillonaria,
+    GranjaPlus,
+    Granjazo,
+    GuacharoActivo,
+    LaGranjita,
+    LaRicachona,
+    LottoActivo,
+    LottoActivoInterRD,
+    LottoRey,
+    Ricachona,
+    SelvaPlus,
+    Terminalito,
+    TrioActivo,
+    TripleCaliente,
+    TripleCaracas,
+    TripleChance,
+    TripleTachira,
+    TripleZamorano,
+    TripleZulia
+)
+
+class Command(BaseCommand):
+    help = 'Elimina registros antiguos de todos los modelos'
+
+    def handle(self, *args, **kwargs):
+        modelos = [
+            ChanceAnimalitos,
+            FruitaGana,
+            GranjaMillonaria,
+            GranjaPlus,
+            Granjazo,
+            GuacharoActivo,
+            LaGranjita,
+            LaRicachona,
+            LottoActivo,
+            LottoActivoInterRD,
+            LottoRey,
+            Ricachona,
+            SelvaPlus,
+            Terminalito,
+            TrioActivo,
+            TripleCaliente,
+            TripleCaracas,
+            TripleChance,
+            TripleTachira,
+            TripleZamorano,
+            TripleZulia
+        ]
+
+        for modelo in modelos:
+            modelo.eliminar_registros_antiguos()
+            self.stdout.write(self.style.SUCCESS(f'Registros antiguos eliminados para {modelo.__name__}'))
